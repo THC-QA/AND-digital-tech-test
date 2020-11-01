a9380d99a8857daa48220d2d0161ba597b75982d
THC-QA <Tristan.Clapp@academytrainee.com>
2020-11-01 21:30:07 +0000

    primary

--- serverless_architecture/modules/route53/main.tf
+++ serverless_architecture/modules/route53/main.tf
@@ -35,7 +35,7 @@ resource "aws_route53_record" "test_route_record" {
   records         = [each.value.record]
   ttl             = 60
   type            = each.value.type
-  zone_id         = aws_route53_zone.test_zone.zone_id
+  zone_id         = aws_route53_zone.primary.zone_id
 }
 resource "aws_acm_certificate_validation" "test_cert_val" {
   certificate_arn         = aws_acm_certificate.test_certificate.arn
