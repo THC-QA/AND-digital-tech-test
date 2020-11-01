a328866a8e050d2fbb43b628b28bfef1bb3a8ea7
THC-QA <Tristan.Clapp@academytrainee.com>
2020-11-01 13:22:14 +0000

    example

--- containerised_architecture/modules/acm_route53/main.tf
+++ containerised_architecture/modules/acm_route53/main.tf
@@ -21,7 +21,7 @@ resource "aws_route53_zone" "test_zone" {
 
 resource "aws_route53_record" "test_route_record" {
   for_each = {
-    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.test_certificate.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
